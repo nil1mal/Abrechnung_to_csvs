9726ffaf2dbd42760181387050d7b82139acb223
nil <nil@mehraslernen.org>
2023-10-04 14:02:45 +0200

    actualizado

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pyxlsb import open_workbook
+import datetime
+
 from datetime import datetime, timedelta
 import logging
 
@@ -23,7 +25,7 @@ def correct_column_date(df, column_name):
 # Function to process and save DataFrame as CSV
 def process_and_save_csv(df, name, type):
     logging.info(f"Processing {name} - {type}")
-    df.to_csv(f"{name}_{type}.csv", sep=';', decimal=',', encoding = "utf-8-sig", index=False)
+    df.to_csv(f"{name}_{type}.csv", sep=';', decimal=',', encoding = "utf-8-sig", index=False, mode='a')
     logging.info(f"{name}_{type}.csv saved successfully")
 
 if __name__ == "__main__":
@@ -32,6 +34,13 @@ if __name__ == "__main__":
     sheet_names = workbook.sheets
 
     for sheet_name in sheet_names:
+
+        try:
+            int(sheet_name)
+        except Exception as error:
+            print("An exception occurred:", type(error).__name__, "–", error)
+            continue
+
         with workbook.get_sheet(sheet_name) as sheet:
             data = [[item.v for item in row] for row in sheet.rows()]
             df = pd.DataFrame(data[1:], columns=data[0])
@@ -40,6 +49,7 @@ if __name__ == "__main__":
 
             # Process data for Einnahme.csv
             einnahme = df[row_index+1:]
+            # einnahme['BUCHUNGSDATUM_ZUKUNFT'] = ""
             einnahme.columns = df.iloc[row_index+1]
             einnahme = einnahme.iloc[1:].dropna(subset=['BELEGNUMMER'], how='all')
             einnahme = correct_column_date(einnahme, "BELEGDATUM")
@@ -48,12 +58,19 @@ if __name__ == "__main__":
             einnahme['PROJEKTNUMMER'] = einnahme['PROJEKTNUMMER'].astype(int)
             einnahme['ZAHLUNGSWEISE'] = einnahme['ZAHLUNGSWEISE'].astype(int)
             einnahme['BETRAG'] = einnahme['BETRAG'].apply(lambda x: x.replace(',', '.') if isinstance(x, str) else x).astype(float)
+            einnahme = einnahme.loc[:, einnahme.columns.notna()]
+
+
             process_and_save_csv(einnahme, sheet_name, "_einnahme")
 
             # Process data for Ausgabe.csv
             ausgabe = df[:row_index].dropna(subset=['BELEGNUMMER'], how='all')
             ausgabe = ausgabe[ausgabe['BETRAG'] != 0.0]
+            ausgabe['BUCHUNGSDATUM_ZUKUNFT'] = ""
+            #print(ausgabe[["BELEGDATUM"]].to_string(index=False))
+
             ausgabe = correct_column_date(ausgabe, "BELEGDATUM")
+
             ausgabe = correct_column_date(ausgabe, "BUCHUNGSDATUM")
             ausgabe = correct_column_date(ausgabe, "ZAHLUNGSDATUM")
             ausgabe = ausgabe.dropna(thresh=ausgabe.shape[1] - 6)
